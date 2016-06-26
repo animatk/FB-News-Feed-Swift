55815e5f0683826ef179306f338eb48b995e0cad
Alejandro St <animatk@gmail.com>
2016-06-26 00:45:28 -0500

    se  crea la estructura de celdas para la collectionView que se puede interpretar como una vista de tabla y que contiene unas filas que son en este caso una clase nueva declarada en el archivo.

--- FbNewsfeed/ViewController.swift
+++ FbNewsfeed/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let cellId = "cellId"
+
 class MainController: UICollectionViewController {
 
 	override func viewDidLoad() {
@@ -17,7 +19,47 @@ class MainController: UICollectionViewController {
 		
 		//titulo de la barra de navegacion
 		navigationItem.title = "Facebook Feed"
+		
+		//se crean los metodos que soportaran la collectionView que es como una especie de tabla que contiene
+		// unos elementos que denominaremos celdas (filas)  que son un elemento que se puede repetir varias veces como una especie de simbolo
+		// el primer metodo (numberOfItemsInSection) retorna el numero de elementos que se repetiran
+		// el segundo (cellForItemAtIndexPath) define la numeracion o identificador de cada celda (fila)
+		
+		//en este paso se agrega las celdas (filas) a la colectionView (tabla)
+		//para ello se crea la estructura de la celda, dicha estructura es una nueva clase que se instancia
+		
+		collectionView?.registerClass(LaCelda.self, forCellWithReuseIdentifier: cellId)
+		
+	}
+	// metodo numberOfItemsInSection
+	override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 3
+	}
+	// metodo cellForItemAtIndexPath
+	override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		return collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
 	}
 
 }
 
+/*
+	Clase que representara la celda (fila) de la presente colectionView
+*/
+
+class LaCelda: UICollectionViewCell {
+	//inicializar clase..
+	override init(frame: CGRect){
+		super.init(frame: frame)
+		
+		setupViews()
+	}
+	
+	//en caso de error
+	required init?(coder aDecoder: NSCoder){
+		fatalError("init(coder:) no esta implementado")
+	}
+	//definicion del la celda, atributos
+	func setupViews() {
+    	backgroundColor = UIColor.whiteColor()
+	}
+}
